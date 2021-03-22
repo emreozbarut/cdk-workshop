65204913ea24ff52da715257883c7e2890cea093
emre.ozbarut <emre.ozbarut@getir.com>
2021-03-22 17:37:39 +0300

    fix

--- src/main/java/com/myorg/WorkshopPipelineStack.java
+++ src/main/java/com/myorg/WorkshopPipelineStack.java
@@ -43,7 +43,7 @@ public class WorkshopPipelineStack extends Stack {
                         .sourceArtifact(sourceArtifact)
                         .cloudAssemblyArtifact(cloudAssemblyArtifact)
                         .buildCommands(Collections.singletonList("mvn clean package"))
-                        .synthCommand("mvn clean package")
+                        .synthCommand("")
                         .build()))
                 .build());
     }
